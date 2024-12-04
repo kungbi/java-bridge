a3fe465cc0281ac90be8a6b316af3e7d0994ac5c
kungbi <kipper12335@gmail.com>
2024-12-04 21:13:24 +0900

    feat: ioc 적용

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,18 +1,12 @@
 package bridge;
 
 import bridge.controller.Controller;
-import bridge.controller.RetryInputUtil;
-import bridge.domain.BridgeMaker;
-import bridge.domain.generator.BridgeRandomNumberGenerator;
 
 public class Application {
 
     public static void main(String[] args) {
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        RetryInputUtil retryInputUtil = new RetryInputUtil(inputView, outputView);
-        Controller controller = new Controller(bridgeMaker, outputView, retryInputUtil);
+        DependencyInjector dependencyInjector = new DependencyInjector();
+        Controller controller = dependencyInjector.createController();
 
         controller.run();
     }
