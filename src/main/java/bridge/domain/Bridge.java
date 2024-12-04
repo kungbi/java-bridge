41cd12bfd98f2659055a52a70510e369bfb9878e
kungbi <kipper12335@gmail.com>
2024-12-04 19:29:02 +0900

    feat(domain): Bridge 에 정적 펙토리 메서드 구현

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -5,12 +5,16 @@ import java.util.List;
 public class Bridge {
     private final List<BridgeCell> bridge;
 
-    public Bridge(List<BridgeCell> bridge) {
+    private Bridge(List<BridgeCell> bridge) {
         if (!(3 <= bridge.size() && bridge.size() <= 20)) {
             throw new IllegalArgumentException("Bridge must contain 3 or more bridges");
         }
         this.bridge = bridge;
     }
 
-
+    public static Bridge from(List<String> bridge) {
+        return new Bridge(
+                bridge.stream().map(BridgeCell::new).toList()
+        );
+    }
 }
